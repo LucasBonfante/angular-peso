b9d0066be55c59ba0d5845b356e11c1f0ce5962e
LucasBonfante <115511797+LucasBonfante@users.noreply.github.com>
2022-10-27 20:30:18 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  public peso: number = 0;
+export class AppComponent {
+  public peso:number = 0;
+  public resposta:string = '';
 
-  exibir(){
+  exibir() {
     console.log('Peso:' + this.peso + 'Kg');
   }
 }
